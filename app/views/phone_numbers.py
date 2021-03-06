2fb04a2b936d809429aec27b26547635a3b4d936
Francisco Pessoa <franciscoaap7@gmail.com>
2021-03-06 14:13:31 -0300

    renamed name of models and added update function

--- app/views/phone_numbers.py
+++ app/views/phone_numbers.py
@@ -1,6 +1,6 @@
 from app import db
 from flask import request, jsonify
-from ..models.phone_numbers import PhoneNumbers, PhoneNumbersSchema
+from ..models.phone_number import PhoneNumber, PhoneNumberSchema
 from flask_sieve import Sieve, Validator
 from sqlalchemy.exc import SQLAlchemyError
 from app.utils import formatResponse
@@ -8,8 +8,8 @@ import math
 
 def show(id):
     try:
-        number = PhoneNumbers.query.filter_by(id=id).first()
-        number_schema = PhoneNumbersSchema()
+        number = PhoneNumber.query.filter_by(id=id).first()
+        number_schema = PhoneNumberSchema()
         result = number_schema.dump(number)
         return formatResponse(result)
     except SQLAlchemyError as e:
@@ -25,11 +25,11 @@ def index():
         return formatResponse("", "pagina e limite são requeridos, limite máximo: 100", 400, True)
 
     try:
-        get_numbers = PhoneNumbers.query.limit(limit).offset((page - 1) * limit).all()
-        numbers_schema = PhoneNumbersSchema(many=True)
+        get_numbers = PhoneNumber.query.limit(limit).offset((page - 1) * limit).all()
+        numbers_schema = PhoneNumberSchema(many=True)
         numbers = dict(
             paginate = numbers_schema.dump(get_numbers),
-            total = PhoneNumbers.query.count()
+            total = PhoneNumber.query.count()
             ) 
         return formatResponse(numbers)
     except SQLAlchemyError as e:
@@ -37,13 +37,13 @@ def index():
 
 def delete(id):    
     try:
-        row = PhoneNumbers.query.filter_by(id=id).first()
+        row = PhoneNumber.query.filter_by(id=id).first()
         if (row is None):
             return formatResponse("", "Não foi encontrado nenhum registro a ser deletado", 400)
 
         db.session.delete(row)
         db.session.commit()
-        return formatResponse("", "Número deletado com sucesso")
+        return formatResponse("", "Número removido com sucesso")
     except SQLAlchemyError as e:
         return formatResponse("", str(e.__dict__['orig']), 500, True)
 
@@ -72,18 +72,18 @@ def store():
         if float(body['setup_price']) < 0:
             return formatResponse("", "O valor de configuração não pode ser negativo", 400)
 
-        row = PhoneNumbers.query.filter_by(value=body['value']).first()
+        row = PhoneNumber.query.filter_by(value=body['value']).first()
         if row:
             return formatResponse("", "Este número já está cadastrado", 400)
 
-        phone_numbers = PhoneNumbers(**body)
+        phone_numbers = PhoneNumber(**body)
 
         try:
             db.session.add(phone_numbers)
             db.session.commit()
-            numbers_schema = PhoneNumbersSchema()
+            numbers_schema = PhoneNumberSchema()
             result = numbers_schema.dump(phone_numbers)
-            return formatResponse(result)
+            return formatResponse(result, "Número inserido com sucesso")
         except SQLAlchemyError as e:
             return formatResponse("", str(e.__dict__['orig']), 500, True)
     return formatResponse(validator.messages(), "validation_errors", 400)
@@ -113,27 +113,31 @@ def update(id):
         if float(body['setup_price']) < 0:
             return formatResponse("", "O valor de configuração não pode ser negativo", 400)
 
-        numbers_schema = PhoneNumbersSchema()
+        try:
+            numbers_schema = PhoneNumberSchema()
 
-        # validation with number exists
-        row            = PhoneNumbers.query.filter_by(value=body['value']).first()
-        data           = numbers_schema.dump(row)
-        if row and data['id'] != id:
-            return formatResponse("", "Este número já está cadastrado", 400)
+            # validation with number exists
+            row            = PhoneNumber.query.filter_by(value=body['value']).first()
+            data           = numbers_schema.dump(row)
+            if row and data['id'] != id:
+                return formatResponse("", "Este número já está cadastrado", 400)
 
-        # validation with number not exists
-        number  = PhoneNumbers.query.filter_by(id=id).first()
-        if (number is None):
-            return formatResponse("", "Número não cadastrado", 400)
+            # validation with number not exists
+            number  = PhoneNumber.query.filter_by(id=id).first()
+            if (number is None):
+                return formatResponse("", "Número não cadastrado", 400)
 
-        # phone_numbers = PhoneNumbers(**body)
-
-        # try:
-        #     db.session.add(phone_numbers)
-        #     db.session.commit()
-        #     numbers_schema = PhoneNumbersSchema()
-        #     result = numbers_schema.dump(phone_numbers)
-        #     return formatResponse(result)
-        # except SQLAlchemyError as e:
-        #     return formatResponse("", str(e.__dict__['orig']), 500, True)
+            dataNumber = PhoneNumber(**body)
+            
+            number.value        = dataNumber.value
+            number.monthy_price = dataNumber.monthy_price
+            number.setup_price  = dataNumber.setup_price
+            number.currency     = dataNumber.currency
+            db.session.commit()
+            numbers_schema = PhoneNumberSchema()
+            result         = numbers_schema.dump(dataNumber)
+            result['id']   = id 
+            return formatResponse(result, "Número atualizado com sucesso")
+        except SQLAlchemyError as e:
+            return formatResponse("", str(e.__dict__['orig']), 500, True)
     return formatResponse(validator.messages(), "validation_errors", 400)
