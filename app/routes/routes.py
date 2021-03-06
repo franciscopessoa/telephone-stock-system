6a0d9b54d441204bc36d2903192bb8f0d9cf961a
Francisco Pessoa <franciscoaap7@gmail.com>
2021-03-06 20:35:40 -0300

    added middleware to verify auth in private routes

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -15,22 +15,27 @@ def login():
 
 # Numbers routes
 @app.route('/number/<int:id>', methods=['GET'])
+@auth.token_required
 def show(id):
     return phone_numbers.show(id)
 
 @app.route('/numbers', methods=['GET'])
+@auth.token_required
 def index():
     return phone_numbers.index()
 
 @app.route('/number/<int:id>', methods=['DELETE'])
+@auth.token_required
 def delete_number(id):
     return phone_numbers.delete(id)
 
 @app.route('/numbers', methods=['POST'])
+@auth.token_required
 def store_number():
     return phone_numbers.store()
 
 @app.route('/number/<int:id>', methods=['PUT'])
+@auth.token_required
 def update_number(id):
     return phone_numbers.update(id)
 
