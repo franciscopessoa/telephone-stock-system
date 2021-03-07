75cc210d7b9b0beacef45e38d348c647628264fa
Francisco Pessoa <franciscoaap7@gmail.com>
2021-03-07 00:17:35 -0300

    added postgres

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database/storage.db'
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
