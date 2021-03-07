93efaf7cd164b6601697d2ea86b972cfde7540c5
Francisco Pessoa <franciscoaap7@gmail.com>
2021-03-07 00:26:19 -0300

    removed migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object('config')
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 ma = Marshmallow(app)
 
